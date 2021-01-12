fd41c033a55e4c64e0096fcd0d3b14f1e2ba090f
jackmcc08 <jackmcc08@gmail.com>
2021-01-12 09:29:38 +0000

    11th Jan 2021 Updates to website

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,19 @@
 //main javascript file for testing
+'use strict'
+
+const switcher = document.querySelector('.btn');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if(className == "light-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+
+    console.log('current class name: ' + className);
+
+});
