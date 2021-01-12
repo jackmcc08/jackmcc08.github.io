2ba22df5f66cc1b7b0c9c60563db85a6230af4ac
jackmcc08 <jackmcc08@gmail.com>
2021-01-12 15:14:57 +0000

    Updates
    Updated across all website to build out basic design and functionality
    
    - HTML - updated main page ot include relevant sections
    - CSS - created new theme (not sure if I like it or not, but good enough for now)
    - JS - updated dark/light button to a toggle, need to improve as don't fully understand how the javascript works.
    - created a resources folder for future resources

--- javascript/app.js
+++ javascript/app.js
@@ -1,12 +1,13 @@
 //main javascript file for testing
 'use strict'
 
-const switcher = document.querySelector('.btn');
+const switcher = document.querySelector('.switch_1');
 
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
 
     var className = document.body.className;
+    //not sure why the below is necessary - needs to investigate
     if(className == "light-theme") {
         this.textContent = "Dark";
     }
